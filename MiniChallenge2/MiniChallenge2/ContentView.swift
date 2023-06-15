873b7bc99c60c021b8d10e0921a07528a0c26d1d
Maria Berliana <mariabyp1617@gmail.com>
2023-06-15 14:38:51 +0700

    test

--- MiniChallenge2/MiniChallenge2/ContentView.swift
+++ MiniChallenge2/MiniChallenge2/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
+                    Text("testing branch")
                 }
                 .onDelete(perform: deleteItems)
             }
